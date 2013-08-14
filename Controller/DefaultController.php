e4afd2c2daed04c890238c8e9fd89d894a584105
Paweł Mikołajczuk <pawel.mikolajczuk@sourcefabric.org>
2013-08-14 10:56:06 +0200

    porting

--- Controller/DefaultController.php
+++ Controller/DefaultController.php
@@ -10,20 +10,130 @@ namespace Newscoop\ArticlesCalendarBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Newscoop\ArticlesCalendarBundle\Entity\ArticleOfTheDay;
+use Newscoop\ArticlesCalendarBundle\Form\Type\ArticleOfTheDayType;
 
 class DefaultController extends Controller
 {
+    /**
+    * @Route("/plugin/articlescalendar/widget")
+    * @Template()
+    */
+    public function widgetAction(Request $request)
+    {  
+        $date = $request->get('date', date('Y/m/d'));
+        $date = explode('/', $date);
+        $today = explode('/', date('Y/m/d'));
+        $view = $request->get('view', 'month');
+
+        if (isset($date[0])) {
+            $year = $date[0];
+        }
+        if (isset($date[1])) {
+            $month = $date[1] - 1;
+        }
+        if (isset($date[2])) {
+            $day = $date[2];
+        } else if (!isset($date[2]) && $view === 'month') {
+            $day = 1;
+        }
+
+        $now = new \DateTime("$today[0]-$today[1]");
+
+        // oldest month user can scroll to YYYY/mm
+        $earliestMonth = $request->get('earliestMonth');
+        if (isset($earliestMonth) && $earliestMonth == 'current') {
+            $earliestMonth = $today;
+        } else if (isset($earliestMonth)) {
+            $earliestMonth = explode('/', $earliestMonth);
+            $tmp_earliest = new \DateTime("$earliestMonth[0]-$earliestMonth[1]");
+
+            if ($tmp_earliest > $now) {
+                $earliestMonth = $today;
+            }
+        } else {
+            $earliestMonth = null;
+        }
+
+        // most recent month user can scroll to YYYY/mm
+        $latestMonth = $request->get('latestMonth', null);
+        if (isset($latestMonth) && $latestMonth == 'current') {
+            $latestMonth = $today;
+        } else if (isset($latestMonth)) {
+            $latestMonth = explode('/', $latestMonth);
+            $tmp_latest = new \DateTime("$latestMonth[0]-$latestMonth[1]");
+
+            if ($now > $tmp_latest) {
+                $latestMonth = $today;
+            }
+        } else {
+            $latestMonth = null;
+        }
+
+        return array(
+            'randomInt' => md5(uniqid('', true)),
+            'today' => explode('/', date('Y/m/d')),
+            'year' => $year,
+            'month' => $month,
+            'day' => $day,
+            'defaultView' => $view,
+            'firstDay' => $request->get('firstDay', 1),
+            'nav' => $request->get('navigation', 'true'),
+            'dayNames' => $request->get('showDayNames'),
+            'earliestMonth' => $earliestMonth,
+            'latestMonth' => $latestMonth,
+        );
+    }
+
     /**
     * @Route("/plugin/articlescalendar/articlesoftheday/get")
     */
     public function getArticlesOfTheDayAction(Request $request)
     {   
         $em = $this->getDoctrine()->getManager();
-        $publication = $request->get('publication');
+        $startDate = $request->get('startDate');
+        $endDate = $request->get('endDate');
+        $imageWidth = $request->get('image_width', 140);
 
-        return new Response('');
+        $articlesOfTheDay = $em->getRepository('Newscoop\ArticlesCalendarBundle\Entity\ArticleOfTheDay')
+            ->createQueryBuilder('a')
+            ->getQuery()
+            ->getResult();
+
+        //get what we need for the json returned data.
+        $results = array();
+
+        foreach ($articlesOfTheDay as $dayArticle) {
+            $element = array();
+            $articleNumber = $dayArticle->getArticle()->getNumber();
+            $image = $this->container->get('image.rendition')->getArticleRenditionImage($articleNumber, 'issuethumb', 140, 94);
+
+            $element['title'] = $dayArticle->getArticle()->getTitle();
+            if (isset($image)) {
+                $element['image'] = $this->container->get('zend_router')->assemble(array('src' => $image['src']), 'image', true, false);
+            } else {
+                $element['image'] = null;
+            }
+
+            $date = $dayArticle->getDate()->format('Y-m-d');
+            $date = explode(" ", $date);
+            $YMD = explode("-", $date[0]);
+
+            $element['date'] = array(
+                "year"=>intval($YMD[0]),
+                "month"=>intval($YMD[1]),
+                "day"=>intval($YMD[2])
+            );
+            $element['url'] = \ShortURL::GetURL($dayArticle->getArticle()->getPublicationId(), $dayArticle->getArticle()->getLanguageId(), null, null, $articleNumber);
+
+            $results[] = $element;
+        }
+
+        return new JsonResponse(array('articles' => $results));
     }
 
     /**
@@ -31,13 +141,54 @@ class DefaultController extends Controller
     */
     public function setArticlesOfTheDayAction(Request $request)
     {   
-        $em = $this->getDoctrine()->getManager();
-        $date = $request->get('date');
-        $articleNumber = $request->get('article_number');
-        $articleLanguage = $request->get('article_language');
+        $em = $this->container->get('em');
+        $form = $this->container->get('form.factory')->create(new ArticleOfTheDayType(), array(), array());
+        $status = false;
 
-        // set article to date
+        if ($request->isMethod('POST')) {
+            $form->handleRequest($request);
 
-        return new Response('');
+            if ($form->isValid()) {
+                $data = $form->getData();
+                $status = true;
+                $articleOfTheDay = $em->getRepository('Newscoop\ArticlesCalendarBundle\Entity\ArticleOfTheDay')
+                    ->createQueryBuilder('a')
+                    ->where('a.articleNumber = :articleNumber')
+                    ->andWhere('a.articleLanguageId = :articleLanguageId')
+                    ->setParameters(array(
+                        'articleNumber' => $data['articleId'],
+                        'articleLanguageId' => $data['articleLanguageId']
+                    ))
+                    ->getQuery()
+                    ->getOneOrNullResult();
+
+                $article = $em->getRepository('Newscoop\Entity\Article')->findOneBy(array(
+                    'language' => $data['articleLanguageId'], 
+                    'number' => $data['articleId']
+                ));
+
+                if ($articleOfTheDay) {
+                    $articleOfTheDay->setDate(new \DateTime($data['custom_date']));
+                } else {
+                    $articleOfTheDay = new ArticleOfTheDay();
+                    $articleOfTheDay->setDate(new \DateTime($data['custom_date']));
+                    $articleOfTheDay->setArticle($article);
+                    $articleOfTheDay->setPublicationId($data['publicationId']);
+                    $em->persist($articleOfTheDay);
+                }
+
+                $em->flush();
+            }
+        }
+
+        return $this->container->get('templating')->renderResponse(
+            'NewscoopArticlesCalendarBundle:Hooks:hook_content.html.twig',
+            array(
+                'article' => $article,
+                'form' => $form->createView(),
+                'status' => $status,
+                'articleOfTheDay' => $articleOfTheDay
+            )
+        );
     }
 }
---
+++ EventListener/HooksListener.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace Newscoop\ArticlesCalendarBundle\EventListener;
+
+use Symfony\Component\HttpFoundation\Request;
+use Newscoop\EventDispatcher\Events\PluginHooksEvent;
+use Newscoop\ArticlesCalendarBundle\Form\Type\ArticleOfTheDayType;
+
+class HooksListener
+{
+    private $container;
+
+    public function __construct($container)
+    {
+        $this->container = $container;
+    }
+
+    public function sidebar(PluginHooksEvent $event)
+    {   
+        $em = $this->container->get('em');
+        $eventArticle = $event->getArgument('article');
+        $article = $em->getRepository('Newscoop\Entity\Article')->findOneBy(array(
+            'language' => $eventArticle->getLanguageId(), 
+            'number' => $eventArticle->getArticleNumber()
+        ));
+
+        $articleOfTheDayDate = $article->getPublishDate();
+        $status = false;
+
+        $articleOfTheDay = $em->getRepository('Newscoop\ArticlesCalendarBundle\Entity\ArticleOfTheDay')
+            ->createQueryBuilder('a')
+            ->where('a.articleNumber = :articleNumber')
+            ->andWhere('a.articleLanguageId = :articleLanguageId')
+            ->setParameters(array(
+                'articleNumber' => $article->getNumber(),
+                'articleLanguageId' => $article->getLanguageId(),
+                'publicationId' => $article->getPublicationId()
+            ))
+            ->getQuery()
+            ->getOneOrNullResult();
+
+        if ($articleOfTheDay) {
+            $status = true;
+            $articleOfTheDayDate = $articleOfTheDay->getDate();
+        }
+
+        $form = $this->container->get('form.factory')->create(new ArticleOfTheDayType(), array(
+            'custom_date' => $articleOfTheDayDate->format('Y-m-d'),
+            'articleId' => $article->getNumber(),
+            'articleLanguageId' => $article->getLanguageId()
+        ), array());
+
+        $response = $this->container->get('templating')->renderResponse(
+            'NewscoopArticlesCalendarBundle:Hooks:sidebar.html.twig',
+            array(
+                'article' => $article,
+                'form' => $form->createView(),
+                'status' => $status, 
+                'articleOfTheDay' => $articleOfTheDay
+            )
+        );
+
+        $event->addHookResponse($response);
+    }
+}
