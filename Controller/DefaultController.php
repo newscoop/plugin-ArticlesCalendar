b2413cf0d55a3887403e889360da046420ef8db1
Rafał Muszyński <rmuszynski1@gmail.com>
2014-03-12 18:03:58 +0100

    added article of the day service

--- Controller/DefaultController.php
+++ Controller/DefaultController.php
@@ -218,6 +218,7 @@ class DefaultController extends Controller
             return $response;
         }
 
+        $articleOfTheDayService = $this->container->get('newscoop_articles_calendar.articles_calendar_service');
         $settings = $em->getRepository('Newscoop\ArticlesCalendarBundle\Entity\Settings')->findOneBy(array(
             'is_active' => true
         ));
@@ -233,22 +234,9 @@ class DefaultController extends Controller
         foreach($publicationNumbers as $value) {
             $query .= "a.publicationNumbers LIKE '". $value ."%' OR ";
         }
-
-        $qb = $em->getRepository('Newscoop\ArticlesCalendarBundle\Entity\ArticleOfTheDay')
-            ->createQueryBuilder('a');
-        $articlesOfTheDay =
-            $qb->select('a.articleNumber', 'a.publicationId', 'a.articleLanguageId', 'a.date', 'aa.name')
-            ->leftJoin('a.article', 'aa')
-            ->where('a.is_active = true')
-            ->andWhere(substr($query, 0, -4))
-            ->andWhere($qb->expr()->between(
-                'a.date',
-                $qb->expr()->literal($start),
-                $qb->expr()->literal($end)
-            ))
-            ->getQuery()
-            ->getArrayResult();
-
+try {
+        $articlesOfTheDay = $articleOfTheDayService->getArticleOfTheDay(new \DateTime($start), new \DateTime($end), $query);
+} catch (\Exception $e) {ladybug_dump($e->getMessage());die;}
         $results = array();
         foreach ($articlesOfTheDay as $dayArticle) {
             $element = array();
